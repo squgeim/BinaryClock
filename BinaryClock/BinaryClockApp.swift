d5a744f1154c88edc4173feecbdbc2b45e66bfb8
squgeim <shreyadahal@gmail.com>
2020-10-31 13:57:37 -0400

    Initial Commit

---
+++ BinaryClock/BinaryClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryClockApp.swift
+//  BinaryClock
+//
+//  Created by Shreya Dahal on 2020-10-31.
+//
+
+import SwiftUI
+
+@main
+struct BinaryClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BinaryClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BinaryClock
+//
+//  Created by Shreya Dahal on 2020-10-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
