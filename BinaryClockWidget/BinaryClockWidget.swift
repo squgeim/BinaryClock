44f979239e3b7c12bdd1917a5c04c35e5c794bd0
squgeim <shreyadahal@gmail.com>
2020-10-31 15:19:58 -0400

    Show binary clock with 1s and 0s

--- BinaryClockWidget/BinaryClockWidget.swift
+++ BinaryClockWidget/BinaryClockWidget.swift
@@ -42,16 +42,65 @@ struct SimpleEntry: TimelineEntry {
 
 func getTimeStr(date: Date) -> String {
     let formatter = DateFormatter()
-    formatter.dateFormat = "hh:mm:ss"
+    formatter.dateFormat = "HHmmss"
     let dateStr = formatter.string(from: date)
 
     return dateStr
 }
 
+func getBitsForChar(digit: Int) -> [Bool] {
+    switch digit {
+    case 0:
+        return [false,false,false,false]
+    case 1:
+        return [false,false,false,true]
+    case 2:
+        return [false,false,true,false]
+    case 3:
+        return [false,false,true,true]
+    case 4:
+        return [false,true,false,false]
+    case 5:
+        return [false,true,false,true]
+    case 6:
+        return [false,true,true,false]
+    case 7:
+        return [false,true,true,true]
+    case 8:
+        return [true,false,false,false]
+    case 9:
+        return [true,false,false,true]
+    default:
+        return [false,false,false,false]
+    }
+}
+
+func getTimeBitList(date: Date) -> [[Bool]] {
+    var bitList: [[Bool]] = []
+
+    let time = getTimeStr(date: date)
+
+    for char in time {
+        let bits = getBitsForChar(digit: Int(String(char)) ?? 0)
+        bitList.append(bits);
+    }
+
+    return bitList
+}
+
 struct BinaryClockWidgetEntryView : View {
     var entry: Provider.Entry
+
     var body: some View {
-        Text(getTimeStr(date: entry.date))
+        HStack {
+            ForEach(getTimeBitList(date: entry.date), id: \.self) { bitList in
+                VStack {
+                    ForEach(bitList, id: \.self) { bit in
+                        bit ? Text("1") : Text("0")
+                    }
+                }
+            }
+        }
     }
 }
 
